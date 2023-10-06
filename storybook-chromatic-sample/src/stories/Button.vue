5eda96a492efb0f2a487abf4be1df10f116ea84e
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2023-10-06 15:02:41 +0900

    chromatic denied 対応

--- storybook-chromatic-sample/src/stories/Button.vue
+++ storybook-chromatic-sample/src/stories/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} woooo</button>
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
 </template>
 
 <script>
