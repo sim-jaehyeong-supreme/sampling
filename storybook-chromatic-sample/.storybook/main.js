b8b69c2eee970e8ff350daf52a6ab09d15fded9d
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2023-10-06 14:47:46 +0900

    change

--- storybook-chromatic-sample/.storybook/main.js
+++ storybook-chromatic-sample/.storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../storybook-chromatic-sample/src/**/*.mdx", "../storybook-chromatic-sample/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
