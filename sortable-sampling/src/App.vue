3253f7fd10eecad7995e5bf9ebb5d41139dc3cbf
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2023-10-25 14:21:05 +0900

    sortable一旦

---
+++ sortable-sampling/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import NestedDraggable from "./components/Nested.vue";
+
+const list = ref([
+  {
+    name: "task 1",
+    tasks: [
+      {
+        type: "left",
+        name: "task 2",
+        tasks: [],
+      },
+      {
+        type: "right",
+        name: "task 3",
+        tasks: [],
+      },
+    ],
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <h3>Nested draggable</h3>
+    <nested-draggable :tasks="list" :length="list.length"/>
+  </div>
+</template>
---
+++ sortable-sampling/src/components/Nested.vue
@@ -0,0 +1,36 @@
+<template>
+  <draggable
+    class="dragArea"
+    tag="ul"
+    :list="tasks"
+    :group="{ name: 'g1' }"
+    item-key="name"
+  >
+    <template #item="{ element }">
+      <li :class="element.type">
+        <p>{{ element.name }}</p>
+        <nested-draggable :tasks="element.tasks" :length="element.tasks.length"/>
+      </li>
+    </template>
+  </draggable>
+</template>
+<script>
+export default {
+  name: "NestedDraggable"
+};
+</script>
+
+<script setup>
+import draggable from "vuedraggable/src/vuedraggable";
+const { length, tasks } = defineProps({
+  length: Number,
+  tasks: Array
+});
+
+</script>
+<style scoped>
+.dragArea {
+  min-height: 50px;
+  outline: 1px dashed;
+}
+</style>
