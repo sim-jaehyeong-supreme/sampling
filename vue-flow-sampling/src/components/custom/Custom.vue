78e994b65e8544178e1f6d6b48b73da777a9220c
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2023-11-06 10:39:03 +0900

    merge

--- vue-flow-sampling/src/components/custom/Custom.vue
+++ vue-flow-sampling/src/components/custom/Custom.vue
@@ -3,10 +3,10 @@ import { VueFlow, useVueFlow } from "@vue-flow/core";
 import { Background } from "@vue-flow/background";
 import { Controls } from "@vue-flow/controls";
 import { MiniMap } from "@vue-flow/minimap";
-import { ref, watch } from "vue";
-import { initialElements } from "@/initial-elements.js";
+import { nextTick, ref, watch } from "vue";
 import CustomSidebar from "@/components/custom/CustomSidebar.vue";
 import CustomPanel from "@/components/custom/CustomPanel.vue";
+import CustomDialog from "@/components/custom/CustomDialog.vue";
 
 defineProps({
   title: String,
@@ -14,14 +14,23 @@ defineProps({
 
 const {
   onPaneReady,
-  onNodeDragStop,
+  // onNodeDragStop,
+  onNodeClick,
   onConnect,
+  findNode,
   addEdges,
-} = useVueFlow();
+  addNodes,
+  project,
+  vueFlowRef,
+} = useVueFlow({nodes: []});
 
-const elements = ref(initialElements);
+// const elements = ref(initialElements);
+// const elements = ref([]);
 const vueFlowInstanceRef = ref(null);
 const isDoneFitView = ref(false);
+const isVisibleDialog = ref(false);
+const currentModel = ref(null);
+
 
 onPaneReady((vueFlowInstance) => {
   vueFlowInstanceRef.value = vueFlowInstance;
@@ -43,30 +52,94 @@ const fitView = () => {
   vueFlowInstanceRef.value.fitView({ padding: 1, includeHiddenNodes: true });
 }
 
-
-onNodeDragStop((e) => {
-  console.log("drag stop", e);
-});
+// onNodeDragStop((e) => {
+//   console.log("drag stop", e);
+// });
 
 onConnect((params) => addEdges(params));
+
+// DnD feature
+const id = ref(0);
+const getNewId = () => `custom_node_${id.value++}`;
+const onDragOver = (event) => {
+  event.preventDefault();
+
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = "move";
+  }
+}
+
+const onDrop = (event) => {
+  const { id, name} = JSON.parse(event.dataTransfer?.getData("application/vueflow"));
+  const { left, top} = vueFlowRef.value.getBoundingClientRect();
+
+  const position = project({
+    x: event.clientX - left,
+    y: event.clientY - top,
+  });
+
+  const newNode = {
+    id: getNewId(),
+    type: "default",
+    position,
+    label: name,
+  };
+
+  addNodes([newNode]);
+
+  nextTick(() => {
+    const node = findNode(newNode.id);
+    const stop = watch(
+      () => node.dimensions,
+      (dimensions) => {
+        if (dimensions.width > 0 && dimensions.height > 0) {
+          node.position = {
+            x: node.position.x - node.dimensions.width / 2,
+            y: node.position.y - node.dimensions.height / 2,
+          };
+          stop();
+        }
+      },
+      { deep: true, flush: "post" }
+    );
+  });
+}
+
+onNodeClick((e) => {
+  console.log("onNodeClick");
+  isVisibleDialog.value = true;
+  currentModel.value = e.node;
+});
+
+const onToggleIsVisibleDialog = (value) => {
+  isVisibleDialog.value = value;
+}
 
 </script>
 
 <template>
-  <div class="title" style="text-align: center">{{ title }}</div>
-  <vue-flow
-    v-model="elements"
-    :class="[isDark ? 'dark' : null]"
-    class="customflow"
-    :default-viewport="{ zoom: 1.5 }"
-    :min-zoom="0.2"
-    :max-zoom="4"
-  >
-    <custom-panel :is-done-fit-view="isDoneFitView" :is-dark="isDark" @toggle-class="toggleClass" @auto-fit="fitView"/>
-    <background :pattern-color="isDark ? '#FFFFFB' : '#aaa'" gap="8" />
-    <mini-map />
-    <controls />
-  </vue-flow>
+  <div class="title" style="text-align: center">{{ title }} </div>
+  <div class="dndflow" @drop="onDrop">
+    <custom-sidebar />
+    <vue-flow
+      :class="[isDark ? 'dark' : null]"
+      class="customflow"
+      :default-viewport="{ zoom: 1.5 }"
+      :min-zoom="0.2"
+      :max-zoom="4"
+      @dragover="onDragOver"
+    >
+      <custom-panel :is-done-fit-view="isDoneFitView" :is-dark="isDark" @toggle-class="toggleClass" @auto-fit="fitView"/>
+      <background :pattern-color="isDark ? '#FFFFFB' : '#aaa'" gap="8" />
+      <mini-map />
+      <controls />
+    </vue-flow>
+  </div>
+  <custom-dialog 
+     v-model:isVisibleDialog="isVisibleDialog"
+     v-model:model="currentModel"
+    @update:isVisibleDialog="onToggleIsVisibleDialog"
+    />
 </template>
 
 <style scoped lang="scss">
@@ -75,6 +148,8 @@ onConnect((params) => addEdges(params));
 }
 
 .customflow {
+  flex-direction: column;
+  display: flex;
   height: 90%;
 }
 
@@ -87,6 +162,47 @@ onConnect((params) => addEdges(params));
   background: #57534e;
   color: #fffffb;
 }
+
+.customflow aside {
+  color: #fff;
+  font-weight: 700;
+  border-right: 1px solid #eee;
+  padding: 15px 10px;
+  font-size: 12px;
+  background: rgba(16, 185, 129, 0.75);
+  -webkit-box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 5px 10px #0000004d;
+}
+.customflow aside .nodes > * {
+  margin-bottom: 10px;
+  cursor: grab;
+  font-weight: 500;
+  -webkit-box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.25);
+  box-shadow: 5px 5px 10px 2px #00000040;
+}
+.customflow aside .description {
+  margin-bottom: 10px;
+}
+.customflow .vue-flow-wrapper {
+  flex-grow: 1;
+  height: 100%;
+}
+@media screen and (min-width: 640px) {
+  .customflow {
+    flex-direction: row;
+  }
+  .customflow aside {
+    min-width: 25%;
+  }
+}
+@media screen and (max-width: 639px) {
+  .customflow aside .nodes {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+  }
+}
+
 .customflow.dark .vue-flow__node {
   background: #292524;
   color: #fffffb;
--- vue-flow-sampling/src/components/custom/CustomSidebar.vue
+++ vue-flow-sampling/src/components/custom/CustomSidebar.vue
@@ -1,8 +1,46 @@
 <script setup>
-const onDragStart = (event, nodeType) => {
+import { ref } from 'vue';
+
+
+const data = ref({
+  dataSource: [
+    {
+      id: 1,
+      name: "source 1",
+    },
+    {
+      id: 2,
+      name: "source 2",
+    },
+    {
+      id: 3,
+      name: "source 3",
+    },
+  ],
+  dataset: [
+  {
+      id: 1,
+      name: "set 1",
+    },
+    {
+      id: 2,
+      name: "set 2",
+    },
+    {
+      id: 3,
+      name: "set 3",
+    },
+  ],
+});
+const onDragStart = (event, nodeType, idx) => {
   if (event.dataTransfer) {
-    event.dataTransfer.setData("application/vueflow", nodeType);
-    event.dataTransfer.effectAllowed = "move";
+    if (nodeType === "dataSource") {
+      event.dataTransfer.setData("application/vueflow", JSON.stringify(data.value.dataSource[idx]));
+      event.dataTransfer.effectAllowed = "move";
+    } else if (nodeType === "dataset") {
+      event.dataTransfer.setData("application/vueflow", JSON.stringify(data.value.dataset[idx]));
+      event.dataTransfer.effectAllowed = "move";
+    }
   }
 };
 </script>
@@ -12,27 +50,40 @@ const onDragStart = (event, nodeType) => {
     <div class="description">You can drag these nodes to the pane.</div>
 
     <div class="nodes">
-      <div
-        class="vue-flow__node-input"
-        :draggable="true"
-        @dragstart="onDragStart($event, 'input')"
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        text-color="#fff"
       >
-        Input Node
-      </div>
-      <div
-        class="vue-flow__node-default"
-        :draggable="true"
-        @dragstart="onDragStart($event, 'default')"
-      >
-        Default Node
-      </div>
-      <div
-        class="vue-flow__node-output"
-        :draggable="true"
-        @dragstart="onDragStart($event, 'output')"
-      >
-        Output Node
-      </div>
+        <el-sub-menu index="1">
+          <template #title>
+            <span>DataSources</span>
+          </template>
+          <template v-for="(item, idx) in data.dataSource" :key="item.id">
+            <div
+              class="vue-flow__node-default"
+              :draggable="true"
+              @dragstart="onDragStart($event, 'dataSource', idx)"
+            >
+              {{ item.name }}
+            </div>
+          </template>
+        </el-sub-menu>
+        <el-sub-menu index="2">
+          <template #title>
+            <span>Datasets</span>
+          </template>
+          <template v-for="(item, idx) in data.dataset" :key="item.id">
+            <div
+              class="vue-flow__node-default"
+              :draggable="true"
+              @dragstart="onDragStart($event, 'dataset', idx)"
+            >
+              {{ item.name }}
+            </div>
+          </template>
+        </el-sub-menu>
+      </el-menu>
     </div>
   </aside>
 </template>
