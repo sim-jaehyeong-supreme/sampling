2e42479d162ac82ca521d31a17c4f0686a013473
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2024-01-30 12:58:39 +0900

    useRefHistory & useManualRefHistory diff is

--- vueuse-history-sampling/src/App.vue
+++ vueuse-history-sampling/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import RefHistory from './components/RefHistory.vue'
+import ManualRefHistory from './components/ManualRefHistory.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ref-history />
+  <manual-ref-history />
 </template>
 
 <style scoped>
