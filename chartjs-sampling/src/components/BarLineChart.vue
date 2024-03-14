3bf6f4e2ec44751db3c946dbba810b695c05d413
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2024-03-14 15:42:00 +0900

    chartjs sampling - bar-line mixed chart

--- chartjs-sampling/src/components/BarLineChart.vue
+++ chartjs-sampling/src/components/BarLineChart.vue
@@ -7,19 +7,21 @@
 <script setup lang="ts">
 import {
   Chart,
-  ChartData,
   ChartDataset,
-  ChartTypeRegistry,
-  DefaultDataPoint,
   registerables,
 } from "chart.js";
-import { BarChart, defineChartComponent } from "vue-chart-3";
+import { defineChartComponent } from "vue-chart-3";
 
 
 Chart.register(...registerables);
 
 const BarLine = defineChartComponent<"bar" | "line">("BarLine", "bar");
 BarLine.id = "bar-line";
+
+const target = 0.3;
+const rest = 0.3;
+const targetValue = 0.55;
+
 
 const data: {
   labels: string[];
@@ -28,16 +30,31 @@ const data: {
   labels: ["Red"],
   datasets: [
     {
-      label: "My First dataset",
-      order: 1,
+      label: "Target",
+      order: 2,
       type: "bar",
-      data: [10]
+      stack: "bar",
+      indexAxis: "y",
+      data: [target],
+      barPercentage: 1,
+      borderRadius: 12,
     },
     {
-      label: "My second one",
+      label: "Rest",
       order: 2,
+      type: "bar",
+      stack: "bar",
+      indexAxis: "y",
+      data: [rest],
+      backgroundColor: "black",
+      borderRadius: 12,
+    },
+    {
+      label: "Target Value",
+      order: 1,
       type: "line",
-      data: [15],
+      data: [targetValue],
+      indexAxis: "y",
       backgroundColor: "#ff0000",
       pointStyle: "rect",
       pointRotation: 45,
